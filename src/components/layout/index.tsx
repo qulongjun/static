fdc06d27cc1fe55c706b62dc1a0c09832ae90f15
jonnyqu <jonnyqu@tencent.com>
2023-02-22 17:25:48 +0800

    feat: add page Layout component

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * @File src/components/layout
+ * @Author author@static.vip
+ * @Date 2023/2/22 17:20:28
+ */
+import React from 'react';
+
+interface ILayout {
+  /* Any Props */
+}
+
+const Layout: React.FC<ILayout> = (props) => {
+
+  return <div>This is layout</div>;
+};
+
+export default Layout;
